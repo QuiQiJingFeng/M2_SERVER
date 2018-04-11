ada30562e36c029e31da824e33357ada42f2534e
JingFeng <gejinnian224@163.com>
2018-04-11 14:40:38 +0800

    更新config文件

---
+++ conf/1/config.lua
@@ -0,0 +1,34 @@
+root = "./"
+luaservice = root.."service/?.lua;"..root.."service/?/init.lua;"..root.."service_center/?.lua;"..root.."service_center/?/init.lua"
+lualoader = root .. "lualib/loader.lua"
+lua_path = root.."lualib/?.lua;"..root.."lualib/?/init.lua;"..root.."service_center/?.lua"
+lua_cpath = root .. "luaclib/?.so"
+snax = root.."test/?.lua"
+cpath = root.."cservice/?.so"
+
+thread = 8
+harbor = 0
+
+logger = "logger"
+logservice = "snlua"
+logpath = "."
+
+start = "main"
+
+protobuf = root.."proto/protocol.pb"
+
+center_redis = "127.0.0.1:6379"
+
+center_mysql = "127.0.0.1:3306"
+
+
+mode = "debug"
+--cluster
+node_type = "center_server"
+node_name = "center_server_01"
+node_address = "127.0.0.1:20001"
+
+server_id = 1
+
+cluster = "./lualib/cluster_config.lua"
+
