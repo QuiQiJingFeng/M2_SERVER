f2374a6d752839a89f40b1756268dc08b5e1e2fc
JingFeng <gejinnian224@163.com>
2018-04-11 14:29:30 +0800

    更新

--- run.sh
+++ run.sh
@@ -1,15 +1,8 @@
+SERVER=$1
 
-COMMON_DIR_PATH="service_common/log/"
-if [ ! -d "$COMMON_DIR_PATH" ]; then
-    mkdir "$COMMON_DIR_PATH"
-fi
 CENTER_DIR_PATH="service_center/log/"
 if [ ! -d "$CENTER_DIR_PATH" ]; then
     mkdir "$CENTER_DIR_PATH"
-fi
-AGENT_DIR_PATH="service_agent/log/"
-if [ ! -d "$AGENT_DIR_PATH" ]; then
-    mkdir "$AGENT_DIR_PATH"
 fi
 
 touch service_common/log/print.log
@@ -38,8 +31,4 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     OSTYPE=cygwin
 fi
 
-nohup ./skynet service_common/config.lua > service_common/log/print.log 2>&1 &
-sleep 0.5
-nohup ./skynet service_center/config.lua > service_center/log/print.log 2>&1 &
-sleep 0.5
-nohup ./skynet service_agent/config.lua > service_agent/log/print.log 2>&1 &
+nohup ./skynet conf/$SERVER/config.lua > conf/$SERVER/print.log 2>&1 &
